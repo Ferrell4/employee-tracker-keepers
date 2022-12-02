2bad33cec8788159d8ffb2c52593da9a8404ad74
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-12-02 16:53:28 -0500

    command prompt creating
    created adaptable code that reacts to different answers

--- index.js
+++ index.js
@@ -13,3 +13,57 @@ const connection = mysql.createConnection(
   },
   console.log("Welcome!")
 );
+// These are the questions that appear on the commandline
+function mainMenu(){
+    inquier
+    .prompt([
+      {
+        type: "list",
+        name: "answer",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Deparments",
+          "Add Department",
+          "Exit",
+        ],
+      },
+    ])
+    // depending on how things are answered, the code switches to the correct line
+    .then(({ answer }) => {
+      switch (answer) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Update Employee Role":
+          updateRole();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "View All Deparments":
+          viewAllDepartments();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Exit":
+          exit();
+          break;
+      }
+    })
+    // for catching errors
+    .catch((err) => {
+      console.log(err);
+    });
+}
