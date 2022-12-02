e8050778ac26eefb7c53f49abbb9f1c39264e079
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-12-02 03:43:27 -0500

    Adding json
    created link to db

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquier = require("inquirer");
 const mysql = require("mysql2");
 require("dotenv").config();
+
+// links database
+const connection = mysql.createConnection(
+  {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  },
+  console.log("Welcome!")
+);
