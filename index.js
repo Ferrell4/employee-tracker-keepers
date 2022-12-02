1d372d902dddb8804baa134f272a679a22f9f0c6
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-12-02 18:55:49 -0500

    Update index.js
    added function for adding employees

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const connection = mysql.createConnection(
   console.log("Welcome!")
 );
 // These are the questions that appear on the commandline
-function mainMenu(){
-    inquier
+function mainMenu() {
+  inquier
     .prompt([
       {
         type: "list",
@@ -70,8 +70,8 @@ function mainMenu(){
 
 // Console  logs a table of all current employees
 function viewAllEmployees() {
-    connection.query(
-      `SELECT 
+  connection.query(
+    `SELECT 
        employee.id, 
        CONCAT (employee.first_name, " ", employee.last_name) AS name, 
        role.title, 
@@ -82,14 +82,67 @@ function viewAllEmployees() {
        JOIN role ON employee.role_id = role.id
        JOIN department ON role.department_id = department.id
        LEFT JOIN employee manager ON employee.manager_id = manager.id`,
-      function (err, result) {
-        if (err) {
-          console.log(err);
-        }
-        console.table(result);
-        mainMenu();
+    function (err, result) {
+      if (err) {
+        console.log(err);
       }
-    );
-  }
+      console.table(result);
+      mainMenu();
+    }
+  );
+}
+
+// For adding employee to data base
+function addEmployee() {
+  inquier
+    .prompt([
+      {
+        type: "input",
+        name: "role",
+        message: "What is your employess role id?",
+      },
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is your employees first name?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is your employees last name?",
+      },
+      {
+        type: "input",
+        name: "manager",
+        message: "what is your employees manager id?",
+      },
+    ])
+    .then((data) => {
+      connection.query(
+        "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)",
+        [data.firstName, data.lastName, data.role, data.manager],
+        function (err, result) {
+          if (err) throw err;
+        }
+      );
+      connection.query(
+        `SELECT 
+                      employee.id, 
+                      CONCAT (employee.first_name, " ", employee.last_name) AS name, 
+                      role.title, 
+                      role.salary,
+                      CONCAT (manager.first_name, " ", manager.last_name) AS manager,
+                      department.name AS department 
+                      FROM employee 
+                       JOIN role ON employee.role_id = role.id
+                       JOIN department ON role.department_id = department.id
+                       LEFT JOIN employee manager ON employee.manager_id = manager.id`,
+        function (err, result) {
+          console.table(result);
+          mainMenu();
+        }
+      );
+    });
+}
 // executes the function
 mainMenu();
